437204d7fb3b2172f0a4579c76240d976f8cabb8
Bharath Bandaru <51408188+Bharath-Bandaru@users.noreply.github.com>
2021-03-29 21:51:06 +0530

    firebase changes

--- config/firebase.js
+++ config/firebase.js
@@ -6,7 +6,7 @@ var serviceAccount = require("../myjson.json");
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://paperclip-285b4.firebaseio.com/"
+  databaseURL: "https://paper-clip-8c79d.firebaseio.com/"
 });
 
 // As an admin, the app has access to read and write all data, regardless of Security Rules
